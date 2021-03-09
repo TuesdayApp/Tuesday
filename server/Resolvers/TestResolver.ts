b9cd1aed38406bd9f8236826ca2bf7a8d538a4ce
Airwarfare <jordanscast@gmail.com>
2021-03-08 20:19:31 -0600

    Linting

--- server/Resolvers/TestResolver.ts
+++ server/Resolvers/TestResolver.ts
@@ -1,9 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Query, Resolver, Int } from "type-graphql";
 
 @Resolver()
 export class TestResolver {
-  @Query(_type => Int)
+  @Query(type => Int)
   theMeaningOfLife(): number {
     return 42;
   }
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,13 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import express from 'express';
-import { TestResolver } from "./Resolvers/TestResolver";
 import dotenv from 'dotenv';
+
+import { TestResolver } from './Resolvers/TestResolver';
+
 dotenv.config()
+
 const app = express();
 
 (async () => {
@@ -18,7 +21,7 @@ const app = express();
     tracing: true
   })
 
-  server.applyMiddleware({ app, path: "/api/graphql" });
+  server.applyMiddleware({ app, path: '/api/graphql' });
 
   app.listen(process.env.PORT, () => console.log(`Api service listening at http://localhost:${process.env.PORT} || http://localhost:${process.env.PORT}${server.graphqlPath}`))
 })()
